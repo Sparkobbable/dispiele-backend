ac06fbb36221f954096da88b6b7a230082cd9070
Jakob <j.wilmesmeier@gmail.com>
2023-11-17 20:47:45 +0100

    test profile

--- src/test/java/de/spielemanufaktur/backend/BackendApplicationTests.java
+++ src/test/java/de/spielemanufaktur/backend/BackendApplicationTests.java
@@ -2,8 +2,10 @@ package de.spielemanufaktur.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BackendApplicationTests {
 
 	@Test
