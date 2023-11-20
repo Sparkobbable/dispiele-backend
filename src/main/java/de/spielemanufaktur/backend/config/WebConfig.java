69c52410b94b30cb29fbd4c58f77e59d00a8d7c8
Jakob <j.wilmesmeier@gmail.com>
2023-11-20 20:43:38 +0100

    move config

--- src/main/java/de/spielemanufaktur/backend/config/WebConfig.java
+++ src/main/java/de/spielemanufaktur/backend/config/WebConfig.java
@@ -1,4 +1,4 @@
-package de.spielemanufaktur.config;
+package de.spielemanufaktur.backend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
