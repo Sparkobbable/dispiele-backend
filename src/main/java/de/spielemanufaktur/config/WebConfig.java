0bb0ba6f3ff39f27971f6b324a932e01ff442804
Jakob <j.wilmesmeier@gmail.com>
2023-11-20 20:20:03 +0100

    enable web mvc for cors

--- src/main/java/de/spielemanufaktur/config/WebConfig.java
+++ src/main/java/de/spielemanufaktur/config/WebConfig.java
@@ -1,14 +1,12 @@
 package de.spielemanufaktur.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
